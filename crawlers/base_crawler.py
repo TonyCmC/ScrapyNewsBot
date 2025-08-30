e594bfb5576ff0e9943c0f952c28eadecaa7b5ce
unknown <engin2005921@gmail.com>
2025-08-31 01:09:31 +0800

    feat: remove chrom driver debug log

--- crawlers/base_crawler.py
+++ crawlers/base_crawler.py
@@ -74,6 +74,8 @@ class NewsCrawler(ABC):
             options.add_argument('--disable-dev-shm-usage')
             options.add_argument('--disable-gpu')
             options.add_argument('--window-size=1920,1080')
+            options.add_argument("--ignore-certificate-errors")
+            options.add_argument("--incognito")
             options.add_argument(f'--user-agent={random.choice(self.USER_AGENTS)}')
             
             driver = webdriver.Chrome(options=options)
